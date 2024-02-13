b9976a0a3372c644b723b73370e48e4582475f47
shivankgoel003 <98269768+shivankgoel003@users.noreply.github.com>
2024-02-12 19:31:44 -0500

    testing done

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -10,6 +10,25 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(testthat)
 
 #### Test data ####
+
+
+#Unit Testing Reference:https://testthat.r-lib.org/reference/test_that.html 
+
+
+#Testing data types
+class(cleaned_state_scoredata$year) == "numeric"
+class(cleaned_state_scoredata$share_inperson) == "numeric"
+class(cleaned_state_scoredata$share_virtual) == "numeric"
+class(cleaned_state_scoredata$share_hybrid) == "numeric"
+class(cleaned_state_scoredata$participation) == "numeric"
+class(cleaned_state_scoredata$pass) == "numeric"
+class(cleaned_state_scoredata$max_cases) == "numeric"
+class(cleaned_state_scoredata$cases_aug) == "numeric"
+class(cleaned_state_scoredata$case_rate_per100k_zip) == "numeric"
+
+class(cleaned_state_scoredata$state) == "character"
+class(colorado$learning_model) == "character"
+class(colorado$learning_model) == "character"
